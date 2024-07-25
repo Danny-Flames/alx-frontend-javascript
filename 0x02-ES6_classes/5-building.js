c7d1a5a8f5a252e6e457520b93eed868da23ec8b
DaNny <danniskolobosiky@gmail.com>
2024-07-25 21:46:08 +0100

    latest changes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,14 @@
-
-/* eslint-disable */
 export default class Building {
-  constructor(sqft) {
-    if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== "number") {
-      throw new TypeError("Sqft must be a number");
-    }
-    this._sqft = sqft;
   }
 }
